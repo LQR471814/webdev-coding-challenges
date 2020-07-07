00d39097f5c0a1cf12155584f81088988bc81030
LQR471814 <bramblefern1013@gmail.com>
2020-07-07 15:09:10 -0700

    Updates

---
+++ precache-manifest.2e0ba16928b2b997012ff00c2164f26a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01d9e473d49b0e3ce92d640f63aeea04",
+    "url": "/webdev-coding-challenges/index.html"
+  },
+  {
+    "revision": "913b6995c331caaa85cd",
+    "url": "/webdev-coding-challenges/static/css/main.551b09e5.chunk.css"
+  },
+  {
+    "revision": "06a469087802432a7585",
+    "url": "/webdev-coding-challenges/static/js/2.b9aeee3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webdev-coding-challenges/static/js/2.b9aeee3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "913b6995c331caaa85cd",
+    "url": "/webdev-coding-challenges/static/js/main.f085661f.chunk.js"
+  },
+  {
+    "revision": "c7cbbc1f4f27dbe6046c",
+    "url": "/webdev-coding-challenges/static/js/runtime-main.17e4a3ad.js"
+  },
+  {
+    "revision": "60e0507498bd7eb738f886baa16c3b55",
+    "url": "/webdev-coding-challenges/static/media/opensans-bold-webfont.60e05074.woff"
+  },
+  {
+    "revision": "cb4b940581f4683b77e0ea292af98ef7",
+    "url": "/webdev-coding-challenges/static/media/opensans-bold-webfont.cb4b9405.woff2"
+  },
+  {
+    "revision": "03c90227356d30a7d49ad63e98a44c17",
+    "url": "/webdev-coding-challenges/static/media/opensans-regular-webfont.03c90227.woff"
+  },
+  {
+    "revision": "abba3dcdc168aa1d4dd98de87d8dc320",
+    "url": "/webdev-coding-challenges/static/media/opensans-regular-webfont.abba3dcd.woff2"
+  }
+]);
